ff7f399f2b6b966d249e93c7ca85679e12a75fe3
Kishan Jaiswal <jaiswalkishan09@gmail.com>
2021-07-13 08:58:28 +0545

    model reg in admin

--- APIProject/api/admin.py
+++ APIProject/api/admin.py
@@ -1,3 +1,20 @@
+ 
 from django.contrib import admin
-
+from api.models import Company, Customers, Employee, Food, FoodDetail,Bill, BillDetails, CompanyAccount, CompanyBank, CustomerRequest, EmploySalary, EmployeeBank
 # Register your models here.
+
+
+admin.site.register(Company)
+admin.site.register(Food)
+admin.site.register(FoodDetail)
+admin.site.register(Employee)
+admin.site.register(Customers)
+admin.site.register(Bill)
+admin.site.register(EmploySalary)
+admin.site.register(BillDetails)
+admin.site.register(CustomerRequest)
+admin.site.register(CompanyAccount)
+admin.site.register(CompanyBank)
+admin.site.register(EmployeeBank)
+
+ 
